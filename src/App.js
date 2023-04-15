1323e4d21edf16f9e6eb447d4e34a8e0004aa410
arjundevlop <arjun.tomar45@gmail.com>
2023-04-15 21:58:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState} from "react";
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [errors, setErrors] = useState([]);
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const errors = [];
+
+    if (!email) {
+      errors.push('Email is required');
+    }
+
+    if (!name) {
+      errors.push('Name is required');
+    }
+
+    if (!password) {
+      errors.push('Password is required');
+    }
+
+    if (!confirmPassword) {
+      errors.push('Confirm password is required');
+    }
+
+    if (password !== confirmPassword) {
+      errors.push('Passwords do not match');
+    }
+    setErrors(errors);
+
+    if (errors.length > 0) {
+      
+      return;
+    }
+
+    setIsSubmitted(true);
+    setEmail('');
+    setName('');
+    setPassword('');
+    setConfirmPassword('');
+
+    
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="signup-form-div">
+    
+    <form className="signup-form" onSubmit={handleSubmit}>
+      <h2>Signup</h2>
+      
+      <input type="text" name='fullName' placeholder="Full Name" value={name} onChange={(event) => setName(event.target.value)} required></input>
+      
+      <input type="email" name='email' placeholder="Email" value={email} onChange={(event) => setEmail(event.target.value)}  required></input>
+      
+      <input type ="password" name='password' placeholder="Password" onChange={(event) => setPassword(event.target.value)} value={password}  required></input>
+      
+      <input type ="password" name='confirmPassword' placeholder="Confirm Password" onChange={(event) => setConfirmPassword(event.target.value)} value={confirmPassword}  required></input>
+      {errors.length>0 ? (
+        errors.map((item)=>{
+          return(<div className='error'>{item}</div>)
+        })
+      ) : "" }
+
+      {isSubmitted && (
+         (<div className='success'>Sign Up successful!</div>)
+      )}
+
+      <button type="submit" onClick={() => setIsSubmitted(false)}>Signup</button>
+     
+    </form>
+    
+      
     </div>
   );
+  
 }
 
 export default App;
